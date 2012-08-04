410d5e9c304ccdff8a2754d192a83e6a1cbea9f9
politzer <politzer@gmail.com>
2012-08-04 00:39:35 -0400

    Fixed home page routing. Done with Ch 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 SampleApp::Application.routes.draw do
   get "users/new"
-  #get "static_pages/home"
 
-  #root :to => 'static_pages#home'
-  match '/home', to: 'static_pages#home'
+  root to: 'static_pages#home'
   match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
